67e976fd7b8998eda753a0e6cc2669d9a7b63732
Adamastor Chimalange <adamchimaj2gmail.com>
2020-08-03 20:31:19 +0100

    Primeira Versao do Programa de a Esterelizacao do Css

---
+++ src/application/Controller.java
@@ -0,0 +1,17 @@
+package application;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+
+public class Controller {
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private MenuBar menuBar;
+
+    @FXML
+    private ScrollPane scrollPane;
+
+}
